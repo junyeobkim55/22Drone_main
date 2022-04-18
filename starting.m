c897d31f151605b7fbe3c7dca72a14bbefd8f99d
Lim-Jong-geon <whdrjs0721@cau.ac.kr>
2022-04-18 22:08:40 +0900

    환전 수정사항 반영했습니다

--- starting.m
+++ starting.m
@@ -1,13 +1,24 @@
-%% 환전 알고리즘 %%
 clc
 clear
+%% KRW
+KRW=input('How much money do you want to exchange(KRW)?: ');
 
-KRW=input('KRW: ');
+%% JPY
 
-% 2022년 4월 18일 기준 1JPY=9.75KRW
-JPY=KRW/9.75
+% JPY Yen Unit
+% 1000 2000 5000 10000
+% 4/18 JPY Exchange Rate 1 JPY = 9.75 KRW
+% JPYER : Exchange Rate in JPY
+JPYER=9.75;
 
-% 2022년 4월 18일 기준 1CNY=193.90KRW
-CNY=KRW/193.90
+JPY=KRW/JPYER;
+%% CNY
 
+% CNY Yuan Unit
+% 1 2 5 10 20 50 100
+% 4/18 CNY Exchange Rate 1 CNY = 193.90 KRW
+% CNYER : Exchange Rate in CNY
 
+CNYER=193.90;
+
+CNY=KRW/CNYER;
