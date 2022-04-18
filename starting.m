75e876228838e5d8498961dd84923a14e68393ab
junyeobkim55 <plumpysound@cau.ac.kr>
2022-04-18 21:20:12 +0900

    원 입력과 달러유로 환전

---
+++ starting.m
@@ -0,0 +1,23 @@
+clc
+clear
+
+%% KRW
+KRW=input('How much KRW? ');
+
+%% USD
+
+% USD Dollar Unit
+% 1, 2, 5, 10, 20 ,50 ,100
+% 4/18 USD Exchange Rate 1 USD = 1,234.30 KRW
+USDER=1234.30;
+
+USD=KRW/USDER;
+
+%% EUR
+
+% EUR Euro Unit
+% 5, 10, 20, 50, 100, 200 ,500
+% 4/18 EUR Exchange Rate 1 EUR = 1,331.90 EUR
+EURER=1331.90;
+
+EUR=KRW/EURER;
