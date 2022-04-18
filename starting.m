d23df8d13ca1d8b76c6bb11299e2f54cc3f14432
Lim-Jong-geon <whdrjs0721@cau.ac.kr>
2022-04-18 22:40:15 +0900

    다시 올립니다

--- starting.m
+++ starting.m
@@ -23,3 +23,23 @@ USD=KRW/USDER;
 EURER=1331.90;
 
 EUR=KRW/EURER;
+%% JPY
+
+% JPY Yen Unit
+% 1000 2000 5000 10000
+% 4/18 JPY Exchange Rate 1 JPY = 9.75 KRW
+% JPYER : Exchange Rate in JPY
+JPYER=9.75;
+
+JPY=KRW/JPYER;
+
+%% CNY
+
+% CNY Yuan Unit
+% 1 2 5 10 20 50 100
+% 4/18 CNY Exchange Rate 1 CNY = 193.90 KRW
+% CNYER : Exchange Rate in CNY
+
+CNYER=193.90;
+
+CNY=KRW/CNYER;
